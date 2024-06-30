814b1443e6bad621622bce4a75bc95a9225dee94
Tatyana Sidorova <tasidorowa@gmail.com>
2024-06-30 22:08:44 +0300

    Чтение массива, вывод массива, подсчет количества элементов массива, меньше или равно 3 символам

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(string[] array)
+{
+    foreach (string item in array)
+    {
+        Console.Write($"{item} ");
+    }
+}
+
+int CountSmallItems(string[] array)
+{
+    int count = 0;
+    foreach (string item in array)
+    {
+        if (item.Length <= 3) count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите массив: ");
+string[] chars = new string[size];
+
+for (int i = 0; i < size; i++)
+{
+    chars[i] = Console.ReadLine();
+}
+
+Console.WriteLine(CountSmallItems(chars));
+
